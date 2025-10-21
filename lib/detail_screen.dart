9ee3e226cbb27b43804cc5e29973a7b3efceca3d
Clemens Mere Krisna Jemarut <clemensmerekrisnajemarut_2327240055@mhs.mdp.ac.id>
2025-10-21 11:28:35 +0700

    Progres wisata_candi
    progres wisata candi tampilkan gambar dan tombol back custom

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
-
+import 'package:wisata_candi_krisna/models/candi.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
+  final Candi candi;
+
+  const DetailScreen({super.key, required this.candi});
 
   @override
   Widget build(BuildContext context) {
-    // return const Placeholder();
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(candi.imageAsset,
+                  width: double.infinity,
+                  height: 300,
+                  fit: BoxFit.cover,
+                  ),
+                ),
+              ),
 
+              //Tombol Back Custom
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 32),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.8),
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.arrow_back),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi_krisna/data/candi_data.dart';
+import 'package:wisata_candi_krisna/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Wisata Candi',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
